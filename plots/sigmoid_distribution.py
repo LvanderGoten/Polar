d301e8d37bdb1bd7c33825606df8aa04fc207f87
Lennart Van der Goten <lvandergoten@gmail.com>
2019-01-02 21:55:20 +0100

    Added new distribution

--- plots/sigmoid_distribution.py
+++ plots/sigmoid_distribution.py
@@ -4,17 +4,46 @@ import numpy as np
 
 EPS = .001
 scales = np.arange(start=1, stop=6)/5
-scales = [10, 20, 30]
+MU = 0
+STD = 1
+A, B = -1, 1
 
 
-def pdf_sigmoid(y, mu=0, std=1):
+def sigmoid(x):
+    return np.reciprocal(1 + np.exp(-x))
+
+
+def pdf_sigmoid_uniform(y, a=A, b=B):
+    return np.where(np.logical_and(sigmoid(a) <= y, y <= sigmoid(b)),
+                    1/(b - a) * 1/(y - np.square(y)),
+                    np.zeros_like(y))
+
+
+def pdf_parametric_uniform(y, scale, a=A, b=B):
+
+    # y smaller than 1/2
+    below_lb = -1/(2 * (scale * a - 1))
+    below_ub = 1/2
+    below_v = 1/(b - a) * 1/(2 * scale * np.square(y))
+    below = np.where(np.logical_and(below_lb <= y, y <= below_ub), below_v, np.zeros_like(y))
+
+    # y larger than 1/2
+    above_lb = 1/2
+    above_ub = (2 * b * scale + 1)/(2 * (b * scale + 1))
+    above_v = 1/(b - a) * 1/(2 * scale * np.square(1 - y))
+    above = np.where(np.logical_and(above_lb <= y, y <= above_ub), above_v, np.zeros_like(y))
+
+    return below + above
+
+
+def pdf_sigmoid_normal(y, mu=MU, std=STD):
     var = std**2
     numerator = np.exp(-np.divide(np.square(np.log(y/(1-y)) - mu), 2 * var))
     denominator = np.sqrt(2 * np.pi * var) * (y - np.square(y))
     return np.divide(numerator, denominator)
 
 
-def pdf_parametric(y, scale, mu=0, std=1):
+def pdf_parametric(y, scale, mu=MU, std=STD):
     var = std**2
 
     # y smaller than 1/2
@@ -37,11 +66,10 @@ def pdf_parametric(y, scale, mu=0, std=1):
 
 
 yy = np.linspace(start=EPS, stop=1-EPS, num=100)
-fy = pdf_sigmoid(yy)
-plt.plot(yy, pdf_sigmoid(yy), label=r"$\sigma$")
+plt.plot(yy, pdf_sigmoid_uniform(yy), label=r"$\sigma(\cdot)$")
 
 for scale in reversed(scales):
-    plt.plot(yy, pdf_parametric(yy, scale=scale), label=r"$\hat{s}_{" + str(scale) + r"}$")
+    plt.plot(yy, pdf_parametric_uniform(yy, scale=scale), label=r"$\hat{s}_{" + str(scale) + r"}$")
 
 plt.xlabel(r"$x$")
 plt.ylabel(r"$f_X(x)$")
